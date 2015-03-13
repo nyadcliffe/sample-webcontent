f874cd99a86d3d08bf7d488467a0bfe774d84a9f
hiroakiabe <hiabe@interlink.ne.jp>
2015-03-13 11:29:51 +0900

    jsのサンプルを微調整

--- htdocs/js/js-sample.js
+++ htdocs/js/js-sample.js
@@ -1,13 +1,6 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /*
  * textareaに入力された文字列数をカウントするJavaScript
  */
-function ShowLength(str) {
+function showLength(str) {
     document.getElementById("inputlength").innerHTML = str.length + "文字";
 }
-
